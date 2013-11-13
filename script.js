533da78b4e2d58c4ec363a7155cd00fc9f705edd
Niki <klaus-martin.hansche@kvberlin.de>
2013-11-13 09:58:05 +0100

    Switch to unminified js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-/* DOKUWIKI:include_once scripts/underscore-min.js */
-/* DOKUWIKI:include_once scripts/raphael-min.js */
-/* DOKUWIKI:include_once scripts/sequence-diagram-min.js */
+/* DOKUWIKI:include_once scripts/underscore.js */
+/* DOKUWIKI:include_once scripts/raphael.js */
+/* DOKUWIKI:include_once scripts/grammar.js */
+/* DOKUWIKI:include_once scripts/diagram.js */
+/* DOKUWIKI:include_once scripts/jquery-plugin.js */
+/* DOKUWIKI:include_once scripts/sequence-diagram.js */
 
 jQuery(
 	function(){
